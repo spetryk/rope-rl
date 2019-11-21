e43acc28db741e0c4be5eb801e8c06adfb219b46
evonneng <evonne_ng@berkeley.edu>
2019-11-21 14:46:38 -0800

    fixed data loader to uniformly sample all o,a pairs

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -12,29 +12,22 @@ class RopeTrajectoryDataset(Dataset):
     """ Rope trajectory dataset """
     def __init__(self, data_dir, transform=None):
         self.data_dir = data_dir
-        self.timestamps = self.filter_trajectories() # list of all valid timestamps
+        self.timestamps, self.depth_list, self.json_list, self.mask_list, self.npy_list = self.filter_trajectories() 
 
     def __getitem__(self, idx):
-        # returns depth and json for random depth/action pair in given idx sequence
-        ts = self.timestamps[idx]
-        # choose random sample in trajectory
-        i = np.random.choice(np.arange(3))
+        depth_path = self.depth_list(idx)
+        json_path = self.json_list(idx)
+        mask_path = self.mask_list(idx)
+        npy_path = self.npy_list(idx)
 
-        idx_prev = 'start' if i == 0 else str(i-1)
-        idx_after = str(i)
-        depth_path = os.path.join(self.data_dir, "depth/", '{}_segdepth_{}.png'.format(ts, idx_prev))
-        json_path = os.path.join(self.data_dir, "json/", '{}_{}.json'.format(ts, idx_after))
-        mask_path = os.path.join(self.data_dir, "mask/", '{}_segmask_{}.png'.format(ts, idx_prev))
-        npy_path = os.path.join(self.data_dir, "npy/", '{}_raw_depth_{}.npy'.format(ts, idx_prev))
-
-        depth_image = cv2.imread(depth_path) # note: rn RGB, change this to grayscale
+        depth_image = cv2.imread(depth_path)
         with open(json_path) as f:
             actions = json.load(f)
 
         return {'depth': depth_image, 'actions': actions}
 
     def __len__(self):
-        return len(self.timestamps)
+        return len(self.depth_list)
 
     def filter_trajectories(self):
         # filter filenames that are incomplete trajectories
@@ -45,10 +38,23 @@ class RopeTrajectoryDataset(Dataset):
         tags = list(filter(lambda x: x is not None, tags))
         tags = [x.groups()[0] for x in tags]
         timestamps = []
+        depth_list = []
+        json_list = []
+        mask_list = []
+        npy_list = []
+
         for ts in tags:
             if tags.count(ts) == 3:
                 timestamps.append(ts)
-        return list(set(timestamps))
+                for i in range(3):
+                    ob_idx = 'start' if i == 0 else str(i-1)
+                    ac_idx = str(i)
+                    depth_list.append(os.path.join(self.data_dir, "depth/", '{}_segdepth_{}.png'.format(ts, ob_idx)))
+                    json_list.append(os.path.join(self.data_dir, "json/", '{}_{}.json'.format(ts, ac_idx)))
+                    mask_list.append(os.path.join(self.data_dir, "mask/", '{}_segmask_{}.png'.format(ts, ob_idx)))
+                    npy_list.append(os.path.join(self.data_dir, "npy/", '{}_raw_depth_{}.npy'.format(ts, ob_idx)))
+
+        return list(set(timestamps)), depth_list, json_list, mask_list, np_list
 
 
 # class MnistDataLoader(BaseDataLoader):
