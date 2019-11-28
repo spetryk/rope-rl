80f2643370a93060fec81d8a1081d5f0da30f81b
evonneng <evonne_ng@utexas.edu>
2019-11-27 19:35:09 -0800

    fixed dataloader triplicating bug

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -52,20 +52,21 @@ class RopeTrajectoryDataset(Dataset):
     def filter_trajectories(self):
         # filter filenames that are incomplete trajectories
         files = os.listdir(os.path.join(self.data_dir, "json"))
-        # extract timestamp
+
+	# extract timestamp
         exp = "(.+)_\d\.json"
         tags = [re.match(exp, f) for f in files]
         tags = list(filter(lambda x: x is not None, tags))
         tags = [x.groups()[0] for x in tags]
-        timestamps = []
+        timestamps = set()
         depth_list = []
         json_list = []
         mask_list = []
         npy_list = []
 
         for ts in tags:
-            if tags.count(ts) == 3:
-                timestamps.append(ts)
+            if tags.count(ts) == 3 and ts not in timestamps:
+                timestamps.add(ts)
                 for i in range(3):
                     ob_idx = 'start' if i == 0 else str(i-1)
                     ac_idx = str(i)
@@ -73,7 +74,7 @@ class RopeTrajectoryDataset(Dataset):
                     json_list.append(os.path.join(self.data_dir, "json/", '{}_{}.json'.format(ts, ac_idx)))
                     mask_list.append(os.path.join(self.data_dir, "mask/", '{}_segmask_{}.png'.format(ts, ob_idx)))
                     npy_list.append(os.path.join(self.data_dir, "npy/", '{}_raw_depth_{}.npy'.format(ts, ob_idx)))
-        
+
         print("dataset_size", len(depth_list))
         return list(set(timestamps)), depth_list, json_list, mask_list, npy_list
 
