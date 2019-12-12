258421d35b0892c5034a3b02501090172b7568e5
JPalakapilly <jjp126@gmail.com>
2019-12-11 19:54:26 -0800

    fixed index bug

--- test.py
+++ test.py
@@ -134,9 +134,9 @@ def eval_model(dataloader, model_path, feat, pretrained, save_dir, size):
             fn = os.path.join(save_dir, f)
             i = f.split("_")[-2]
             if i == "start":
-                i = 1
+                i = 0
             else:
-                i = int(i) + 1
+                i = int(i)
 
             plt.savefig('{}_points.png'.format(fn))
             print('saving plot to:', '{}_points.png'.format(fn))
@@ -157,9 +157,9 @@ def eval_model(dataloader, model_path, feat, pretrained, save_dir, size):
         plt.xlim(.2, .65)
         plt.ylim(-.2, .35)
         if pretrained:
-            plt.title("ResNet18" + pretrained_title + feat_titles[feat] + size_titles[size] + "action: " + str(i))
+            plt.title("ResNet18" + pretrained_title + feat_titles[feat] + size_titles[size] + "; action: " + str(i+1))
         else:
-            plt.title("ResNet18" + pretrained_title + feat_titles[feat] + size_titles[size] + "action: " + str(i))
+            plt.title("ResNet18" + pretrained_title + feat_titles[feat] + size_titles[size] + "; action: " + str(i+1))
         plt.scatter(target_grasp_x[i], target_grasp_y[i], c='r', marker='o', label="ground truth grasp") # grasp [target]
         plt.scatter(target_drop_x[i], target_drop_y[i], c='r', marker='x', label="ground truth drop") # drop [target]
         plt.scatter(predicted_grasp_x[i], predicted_grasp_y[i], c='b', marker='o', label="predicted grasp") # grasp [pred]
@@ -178,14 +178,14 @@ if __name__ == '__main__':
     # vis-descriptor specific file paths
     parser.add_argument('--config', default='cfg', type=str,
                       help='path to config')
-    parser.add_argument('--test_dir', default='data/val/', type=str,
-                      help='directory to validation data')
+    parser.add_argument('--test_dir', default='data/val', type=str,
+                      help='directory to testing data')
     parser.add_argument('--network_dir', default='/nfs/diskstation/priya/rope_networks/', type=str,
                       help='directory to vis_descriptor network')
     parser.add_argument('--network', default='rope_noisy_1400_depth_norm_3', type=str,
                       help='filename of vis_descriptor network')
     parser.add_argument('--save_dir', default='visualizations/test/', type=str,
-                      help='directory to validation data')
+                      help='directory to save plots')
     
     # training specific arguments
     parser.add_argument('--model_dir', default='bc_model/', type=str,
